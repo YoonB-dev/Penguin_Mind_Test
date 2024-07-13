3434e4bfe1f0803ef24d3602c96b69741ae47912
Yoon <151509222+yoonB-dev@users.noreply.github.com>
2024-07-13 14:22:15 +0900

    pen_mind_test

--- js/share.js
+++ js/share.js
@@ -4,8 +4,7 @@ function setShare(){
     var resultAlt = resultImg.firstElementChild.alt;
     const shareTitle = '펭귄으로 보는 심리 테스트 결과';
     const shareDesc = infoList[resultAlt].name;
-    const imgAlt = resultAlt+'1';
-    const shareImg = url + 'img/pen' + imgAlt + '.png';
+    const shareImg = url + 'img/pen' + parseInt(resultAlt)+1 + '.png';
     const shareURL = url + 'page/result_' + resultAlt + '.html';
 
     Kakao.Share.sendDefault({
